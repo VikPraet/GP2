a28c7b502cc37fcbd14a5658d22909a9607f7b6c
Vik Praet <vik.praet@gmail.com>
2024-03-20 23:10:54 +0100

    updated to use staging buffer

--- VulkanGameEngine/Project/Mesh.h
+++ VulkanGameEngine/Project/Mesh.h
@@ -21,7 +21,13 @@ namespace vve
 			static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
 		};
 
-		VveMesh(VveDevice& device, const std::vector<Vertex>& vertices);
+		struct Builder
+		{
+			std::vector<Vertex> vertices{};
+			std::vector<uint32_t> indices{};
+		};
+
+		VveMesh(VveDevice& device, const VveMesh::Builder& builder);
 		~VveMesh();
 
 		VveMesh(const VveMesh&) = delete;
@@ -32,10 +38,17 @@ namespace vve
 
 	private:
 		void createVertexBuffers(const std::vector<Vertex>& vertices);
+		void createIndexBuffers(const std::vector<uint32_t>& indices);
 
 		VveDevice& m_Device;
+
 		VkBuffer m_VertexBuffer;
 		VkDeviceMemory m_VertexBufferMemory;
 		uint32_t m_VertexCount;
+
+		bool m_HasIndexBuffer = false;
+		VkBuffer m_IndexBuffer;
+		VkDeviceMemory m_IndexBufferMemory;
+		uint32_t m_IndexCount;
 	};
 }
